03ebc4ad5f048a0a46a94beaf25a6baa30e0496f
Arthur Heymans <arthur@aheymans.xyz>
2024-12-03 21:55:26 +0100

    Add Elysium
    This does not work well...
    
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>

--- packages.el
+++ packages.el
@@ -15,6 +15,8 @@
   :recipe (:host github :repo "tninja/aider.el" :files ("aider.el"))
   :pin "fe6148c0648f07293a047d1c687944d33c40f4e4")
 
+(package! elysium)
+
 (package! kconfig-mode
   :recipe (:host github :repo "delaanthonio/kconfig-mode")
   :pin "cd87b71c8c1739d026645ece0bbd20055a7a2d4a")
